26ebe6efe0d9a4e6ad71bac2547c63e13eb28d84
Risheng1128 <hi4u29ck02@gmail.com>
2022-03-07 17:42:35 +0800

    Add the solution of Remove Duplicates from Sorted List II (Not done)

---
+++ quiz1/problem2.c
@@ -0,0 +1,33 @@
+/**
+ * @file    problem3.c
+ * @brief   實作 82. Remove Duplicates from Sorted List II (https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/)
+ */
+
+#include <stdio.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+
+struct ListNode *deleteDuplicates(struct ListNode *head)
+{
+    if (!head)
+        return NULL;
+
+    if (head->next && head->val == head->next->val) {
+        /* Remove all duplicate numbers */
+        while (head->next && head->val == head->next->val)
+            head = head->next;
+        return deleteDuplicates(head->next);
+    }
+
+    head->next = deleteDuplicates(head->next);
+    return head;
+}
+
+int main(void)
+{
+    return 0;
+}
