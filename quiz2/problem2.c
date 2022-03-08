425b82d2fe6125c0c57243708e738680d7d65cf8
Risheng1128 <hi4u29ck02@gmial.com>
2022-03-08 23:13:02 +0800

    Add the solution of problem2

---
+++ quiz2/problem2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdint.h>
+
+/** 
+ * @fn     - max_uint32
+ * @brief  - 找到無號整數比較大的數
+ * 
+ */
+uint32_t max_uint32(uint32_t a, uint32_t b)
+{
+    return a ^ ((a ^ b) & -(a < b));
+}
+
+/** 
+ * @fn     - max_int32
+ * @brief  - 找到有號整數比較大的數
+ * 
+ */
+int32_t max_int32(int32_t a, int32_t b)
+{
+    return a ^ ((a ^ b) & -(a < b));
+}
+
+int main(void)
+{
+    uint32_t a1 = 3, b1 = 5;
+    int32_t a2 = -1, b2 = 1;
+    printf("max1 = %u\n", max_uint32(a1, b1));
+    printf("max1 = %d\n", max_int32(a2, b2));
+
+    return 0;
+}
