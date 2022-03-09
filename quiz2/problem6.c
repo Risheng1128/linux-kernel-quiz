19ff53b80fb3091802f1c2eab4a80bd0ca7785b7
Risheng1128 <hi4u29ck02@gmial.com>
2022-03-10 05:12:33 +0800

    Create the solution of problem6.c

---
+++ quiz2/problem6.c
@@ -0,0 +1,24 @@
+/**
+ * @file    problem6.c
+ * @brief   類似 __alignof__ 的實作
+ */
+
+#include <stdio.h>
+
+/*
+ * ALIGNOF - get the alignment of a type
+ * @t: the type to test
+ *
+ * This returns a safe alignment for the given type.
+ */
+#define ALIGNOF(t) \
+    ((char *)(&((struct { char c; t _h; } *)0)->_h) - (char *)0)
+
+int main(void)
+{
+    printf("%ld\n", ALIGNOF(char));
+    printf("%ld\n", ALIGNOF(int));
+    printf("%ld\n", ALIGNOF(float));
+    printf("%ld\n", ALIGNOF(double));
+    return 0;
+}
