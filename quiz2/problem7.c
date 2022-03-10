6805f34283d65e59b3db4da0765ab2d5ece942fb
Risheng1128 <hi4u29ck02@gmial.com>
2022-03-10 23:24:53 +0800

    Add the solution of problem7

--- quiz2/problem7.c
+++ quiz2/problem7.c
@@ -1,6 +1,12 @@
+/**
+ * @file    problem7.c
+ * @brief   FizzBuzz (https://en.wikipedia.org/wiki/Fizz_buzz) 兩種 (naive, bitwise) 不同的實作
+ */
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
 
 void naive(void)
 {
@@ -13,6 +19,18 @@ void naive(void)
     }
 }
 
+/** 
+ * @fn     - is_divisible
+ * @brief  - 回傳是否可以整除，可以回傳 true, 反則回傳 false
+ * 
+ * @attention 函式邏輯
+ * 1. M3 除以 3 表示以 3 為循環，而 +1 是為了要讓乘上 3 的倍數時會 overflow ，而產生一個很小的數
+ * 2. M5 除以 5 表示以 5 為循環，而 +1 是為了要讓乘上 5 的倍數時會 overflow ，而產生一個很小的數
+ * 3. 搭配以上兩點，我們可以得知 is_divisible() 的實作手法，利用 overflow 判斷是否可以整除
+ *    - 當 M 為 M3 時，如果 n * M 產生 overflow 時，表示 n 可以被 3 整除，此時 n * M 是一個很小的數，因此 n * M <= M - 1 也就成立，回傳 true
+ *    - 當 M 為 M5 時，如果 n * M 產生 overflow 時，表示 n 可以被 5 整除，此時 n * M 是一個很小的數，因此 n * M <= M - 1 也就成立，回傳 true
+ *    - 其他的情況則回傳 false
+ */
 static inline bool is_divisible(uint32_t n, uint64_t M)
 {
     return n * M <= M - 1;
@@ -26,21 +44,22 @@ void bitwise(void)
     for (size_t i = 1; i <= 100; i++) {
         uint8_t div3 = is_divisible(i, M3);
         uint8_t div5 = is_divisible(i, M5);
-        unsigned int length = (2 << KK1) << KK2;
+        unsigned int length = (2 << div3) << div5;
+        printf("length = %d\n", length);
 
         char fmt[9];
-        strncpy(fmt, &"FizzBuzz%u"[(9 >> div5) >> (KK3)], length);
+        strncpy(fmt, &"FizzBuzz'%u'"[(9 >> div5) >> (div3 << 2)], length);
         fmt[length] = '\0';
 
         printf(fmt, i);
         printf("\n");
     }
-    return 0;
 }
 
 int main()
 {
-    naive();
+    // naive();
+    bitwise();
     return 0;
 }
 
