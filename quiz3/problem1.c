3069890c04ca94294cc0ee33f69c9f73077c0d8c
Risheng1128 <hi4u29ck02@gmail.com>
2022-03-11 14:25:41 +0800

    Creat the solution of problem1

---
+++ quiz3/problem1.c
@@ -0,0 +1,17 @@
+/**
+ * @file    - problem1.c
+ * @brief   - 練習實作 linux/include/linux/bitfield.h 的 GENMASK 巨集函式 
+ *            (https://github.com/torvalds/linux/blob/master/include/linux/bitfield.h)
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+
+#define GENMASK(h, l) \
+    (((~0UL) >> (63 - h)) & ((~0UL) >> (l) << (l)))
+
+int main(void)
+{
+    printf("%lx\n", GENMASK(63, 0));
+    return 0;
+}
