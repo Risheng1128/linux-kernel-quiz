96d43e1abf4750f7f4110340bf614e3cd60a240d
Risheng1128 <hi4u29ck02@gmial.com>
2022-03-23 05:25:27 +0800

    Complete the solution of problem2

--- quiz3/problem2.c
+++ quiz3/problem2.c
@@ -3,8 +3,9 @@
  * @brief   - 函式 fo_fd 接受指定的地址 v，填入一個 fd 結構體，並確保第一個成員的地址得以對 4 個位元組進行向下對齊
  */
 
+#include <stdio.h>
+
 struct foo;
-  
 struct fd {
     struct foo *foo;
     unsigned int flags;
@@ -18,11 +19,13 @@ enum {
 
 static inline struct fd to_fd(unsigned long v)
 {
-    return (struct fd){(struct fd *) (v & ~3), v & 3};
+    return (struct fd){(struct foo *) (v & ~3), v & 3};
 }
 
 int main(void)
 {
+    unsigned long address = 0xFFFF;
+    printf("%p\n", to_fd(address).foo);
     return 0;
 }
 
