a8b4a9c2a707cb249c273b56053c0731ba264525
Risheng1128 <hi4u29ck02@gmail.com>
2022-03-31 17:33:04 +0800

    Complete problem10

--- quiz3/problem10.c
+++ quiz3/problem10.c
@@ -3,12 +3,21 @@
  * @brief   - 以下巨集可得到二個表示式進行整數除法操作時，最接近的整數值
  */
 
+#include <stdio.h>
+
 #define DIVIDE_ROUND_CLOSEST(x, divisor)                       \
     ({                                                         \
         typeof(x) __x = x;                                     \
         typeof(divisor) __d = divisor;                         \
         (((typeof(x)) -1) > 0 || ((typeof(divisor)) -1) > 0 || \
          (((__x) > 0) == ((__d) > 0)))                         \
-            ? ((RRR) / (__d))                  \
-            : ((SSS) / (__d));                 \
+            ? (((__x) + ((__d) >> 1)) / (__d))                \
+            : (((__x) - ((__d) >> 1)) / (__d));               \
     })
+
+int main(void)
+{
+    printf("%d\n", DIVIDE_ROUND_CLOSEST(-10, 3)); // -3
+    printf("%d\n", DIVIDE_ROUND_CLOSEST(-11, 3)); // -4
+    return 0;
+}
