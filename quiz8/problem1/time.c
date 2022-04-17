2342c7548ff5c2fe7f8b2c2916d375bb134cbbe4
Risheng1128 <hi4u29ck02@gmail.com>
2022-04-18 06:32:52 +0800

    Create file to measure time

---
+++ quiz8/problem1/time.c
@@ -0,0 +1,46 @@
+/**
+ * @file    - time.c
+ * @brief   - 量測不同平台 memchr 的實作所花的時間
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include "memchr.h"
+#include "opt.h"
+
+#define STRSIZE 1000U
+
+long long elapse(struct timespec *start, struct timespec *end)
+{
+    return (long long) (end->tv_sec - start->tv_sec) * 1e9 + (long long) (end->tv_nsec - start->tv_nsec);
+}
+
+int main(void)
+{
+    char str[STRSIZE];
+    const char target = '4';
+    struct timespec start, end;
+    long long origin_time = 0, opt_time;
+
+    memset(str, '0', STRSIZE - 1);
+    str[STRSIZE - 1] = '\0';
+    
+    for (int i = 0; i < STRSIZE - 1; i++) {
+        str[i] = target;
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        memchr_origin(str, target, STRSIZE - 1);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        origin_time = elapse(&start, &end);
+
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        memchr_opt(str, target, STRSIZE - 1);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        opt_time = elapse(&start, &end);
+
+        printf("%lld %lld\n", origin_time, opt_time);
+        origin_time = opt_time = 0;
+        str[i] = '0';
+    }
+    return 0;
+}
